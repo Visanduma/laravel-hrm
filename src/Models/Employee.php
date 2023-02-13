efb67bf42116d94e4d3ec1c2845c9e5c427f4f1a
dhanuSama <dhanusha@visanduma.com>
2023-02-13 11:45:39 +0530

    Employee relationships

--- src/Models/Employee.php
+++ src/Models/Employee.php
@@ -9,4 +9,24 @@ class Employee extends Model
     protected $table = 'hrm_employees';
 
     protected $guarded = [];
+
+    public function empType()
+    {
+        return $this->belongsTo(EmployementType::class, 'emp_type_id');
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'emp_branch_id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'emp_dept_id');
+    }
+
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class, 'emp_desig_id');
+    }
 }
