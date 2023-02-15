be5c2ec6a598a6f99d1c318698d7205fa2ac6a85
dhanuSama <dhanusha@visanduma.com>
2023-02-15 12:01:39 +0530

    Employee personal

--- src/Models/Employee.php
+++ src/Models/Employee.php
@@ -10,6 +10,10 @@ class Employee extends Model
 
     protected $guarded = [];
 
+    protected $appends = ['first_name', 'middle_name', 'last_name', 'addressLine1', 'addressLine2', 'addressLine3', 'dob', 'nic', 'mobile'];
+
+    // Relationships
+
     public function empType()
     {
         return $this->belongsTo(EmployementType::class, 'emp_type_id');
@@ -28,5 +32,57 @@ class Employee extends Model
     public function designation()
     {
         return $this->belongsTo(Designation::class, 'emp_desig_id');
+    }
+
+    public function personal()
+    {
+        return $this->hasMany(EmployeePersonal::class, 'emp_id', 'id');
+    }
+
+    // Custom Attributes
+
+    public function getFirstNameAttribute()
+    {
+        return $this->personal()->where('field', 'first_name')->first()->value ?? null;
+    }
+
+    public function getMiddleNameAttribute()
+    {
+        return $this->personal()->where('field', 'middle_name')->first()->value ?? null;
+    }
+
+    public function getLastNameAttribute()
+    {
+        return $this->personal()->where('field', 'last_name')->first()->value ?? null;
+    }
+
+    public function getAddressLine1Attribute()
+    {
+        return $this->personal()->where('field', 'addressLine1')->first()->value ?? null;
+    }
+
+    public function getAddressLine2Attribute()
+    {
+        return $this->personal()->where('field', 'addressLine2')->first()->value ?? null;
+    }
+
+    public function getAddressLine3Attribute()
+    {
+        return $this->personal()->where('field', 'addressLine3')->first()->value ?? null;
+    }
+
+    public function getDobAttribute()
+    {
+        return $this->personal()->where('field', 'dob')->first()->value ?? null;
+    }
+
+    public function getNicAttribute()
+    {
+        return $this->personal()->where('field', 'nic')->first()->value ?? null;
+    }
+
+    public function getMobileAttribute()
+    {
+        return $this->personal()->where('field', 'mobile')->first()->value ?? null;
     }
 }
--- src/Models/EmployeePersonal.php
+++ src/Models/EmployeePersonal.php
@@ -3,10 +3,24 @@
 namespace Visanduma\LaravelHrm\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Visanduma\LaravelHrm\Models\Employee;
 
 class EmployeePersonal extends Model
 {
     protected $table = 'hrm_employee_personal';
 
     protected $guarded = [];
+
+    public static function insertUpdateBulk(Employee $employee, $request)
+    {
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'first_name'], ['value' => $request->first_name]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'middle_name'], ['value' => $request->middle_name]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'last_name'], ['value' => $request->last_name]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'dob'], ['value' => $request->dob]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine1'], ['value' => $request->addressLine1]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine2'], ['value' => $request->addressLine2]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine3'], ['value' => $request->addressLine3]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'nic'], ['value' => $request->nic]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'mobile'], ['value' => $request->mobile]);
+    }
 }
