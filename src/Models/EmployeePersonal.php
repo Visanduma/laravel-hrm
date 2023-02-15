39caf46fb7ce07035d92a5c172e6e614e0847cf7
dhanuSama <dhanuSama@users.noreply.github.com>
2023-02-15 06:32:19 +0000

    Fix styling

--- src/Models/EmployeePersonal.php
+++ src/Models/EmployeePersonal.php
@@ -3,7 +3,6 @@
 namespace Visanduma\LaravelHrm\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Visanduma\LaravelHrm\Models\Employee;
 
 class EmployeePersonal extends Model
 {
@@ -14,13 +13,13 @@ class EmployeePersonal extends Model
     public static function insertUpdateBulk(Employee $employee, $request)
     {
         EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'first_name'], ['value' => $request->first_name]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'middle_name'], ['value' => $request->middle_name]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'last_name'], ['value' => $request->last_name]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'dob'], ['value' => $request->dob]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine1'], ['value' => $request->addressLine1]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine2'], ['value' => $request->addressLine2]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'addressLine3'], ['value' => $request->addressLine3]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'nic'], ['value' => $request->nic]);
-        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id,'field' => 'mobile'], ['value' => $request->mobile]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'middle_name'], ['value' => $request->middle_name]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'last_name'], ['value' => $request->last_name]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'dob'], ['value' => $request->dob]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'addressLine1'], ['value' => $request->addressLine1]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'addressLine2'], ['value' => $request->addressLine2]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'addressLine3'], ['value' => $request->addressLine3]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'nic'], ['value' => $request->nic]);
+        EmployeePersonal::updateOrCreate(['emp_id' => $employee->id, 'field' => 'mobile'], ['value' => $request->mobile]);
     }
 }
